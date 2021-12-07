aa570c70bfdbfe04c5cdd8385665654008872b43
Michael Bonilla <michaelbonilla13@hotmail.com>
2021-12-07 17:59:24 -0500

    CRUD table Image

--- src/image/entities/Image.ts
+++ src/image/entities/Image.ts
@@ -1,7 +1,9 @@
 interface Image {
-  _id: number,
+  _id: string,
   photo: string,
   __v: number
 }
 
 export default Image;
+
+export type AddImageParams = Omit<Image, 'id' | '__v'>
--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,12 @@
 // eslint-disable-next-line import/no-import-module-exports
 import express from 'express';
 import ClientRouter from './client/frameworks-drivers/routes/clientRoutes';
+import Mongo from './image/frameworks-drivers/ormMongoose/conection';
+import ImageRouter from './image/frameworks-drivers/routes/imageRoutes';
 
 const path = require('path');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const mongoose = require('mongoose');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
@@ -21,22 +22,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.resolve(__dirname, '../public')));
 
-mongoose.connect(
-  process.env.ATLAS_URI,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-);
-
-const { connection } = mongoose;
-
-connection.once('open', () => {
-  console.log('MongoDB database connection established succesfully');
-});
+Mongo.init();
 
 app.use('/api/V1/cliente', ClientRouter);
-// app.use('/api/V1/imagen', require('./routes/image'));
+app.use('/api/V1/imagen', ImageRouter);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
